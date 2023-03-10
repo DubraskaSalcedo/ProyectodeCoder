44385ccb64138eacc38be48633818c27f89fc4d4
Dubraska Salcedo <dubraskasalcedo@gmail.com>
2023-03-10 11:05:19 -0300

    Agregamos botones en barra de navegación y cada uno de ellos lleva a una vista diferente

--- AppCoderone/views.py
+++ AppCoderone/views.py
@@ -3,7 +3,17 @@ from django.http import HttpResponse
 from AppCoderone.models import Curso
 # Create your views here.
 
-def guardar_curso(request, camada):
-    curso = Curso(nombre="Python", camada= camada)
-    curso.save()
-    return HttpResponse("Curso creado exitosamente")
+def cursos(request):
+    all_cursos = Curso.objects.all()
+    context = {"cursos": all_cursos}
+    return render(request, "AppCoderone/cursos.html", context=context)
+
+def crear_curso(request, nombre, camada):
+    save_curso=Curso(nombre=nombre, camada= int(camada))
+    save_curso.save()
+    context= {"nombre": nombre}
+    return render(request, "AppCoderone/save_curso.html", context=context)
+def estudiantes(request):
+    return render(request, "AppCoderone/estudiantes.html")
+def profesores(request):
+    return render(request, "AppCoderone/profesores.html")
